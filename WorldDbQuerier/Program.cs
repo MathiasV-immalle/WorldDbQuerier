3452403073c52d8ba7993a5ebcb966681b66ecfe
Mathias Vansant <mathias.vansant@immalle.eu>
2017-09-28 09:25:19 +0200

    Working parameter -v Working error for non existing parameters such as -a Colors added

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -4,10 +4,29 @@ namespace WorldDbQuerier
 {
     class Program
     {
+        static string version = "0.1";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("WorldDbQuerier 0.1!");
-
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to WorldDbQuerier");
+            Console.ForegroundColor = ConsoleColor.White;
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-v":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Version {0}", version);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Onbekend argument");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            }
         }
     }
 }
