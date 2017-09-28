601c80d565d7daf41b777d6d52d0516171bae545
Mathias Vansant <mathias.vansant@immalle.eu>
2017-09-28 09:50:14 +0200

    Update to Version 0.2: Connection made to MySql DataBase in Xubuntu First Query run

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-
+using MySql.Data.MySqlClient;
 namespace WorldDbQuerier
 {
     class Program
     {
-        static string version = "0.1";
+        static string version = "0.2";
 
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Welcome to WorldDbQuerier");
-            Console.ForegroundColor = ConsoleColor.White;
+            PrintVerwelkoming();
             if (args.Length > 0)
             {
                 switch (args[0])
@@ -27,6 +25,36 @@ namespace WorldDbQuerier
                         break;
                 }
             }
+            PrintMenu();
+            MaakConnectie();
+        }
+
+        static void PrintVerwelkoming() {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to WorldDbQuerier");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void PrintMenu() {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("menu coming");
+        }
+
+        static void MaakConnectie()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+                "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=imma;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT count(Name) FROM Country";
+
+            conn.Open();
+
+            int AantalLanden = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine("Aantal landen : {0}", AantalLanden);
         }
     }
 }
